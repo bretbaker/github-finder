b03eb1d97702424b703db80041258b60f35ca65e
Bret Baker <bretbaker808@gmail.com>
2020-01-07 13:17:24 -0800

    initial commit

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        className='round-img'
+        src={avatar_url}
+        style={{ width: '60px' }}
+        alt=''
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          className='btn btn-dark btn-sm my-1'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default UserItem;
