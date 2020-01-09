9e164c55237179286eab3931777a7f975d95d47f
Bret Baker <bretbaker808@gmail.com>
2020-01-09 09:24:28 -0800

    app done, ready for deploy

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -5,7 +5,12 @@ const RepoItem = ({ repo }) => {
   return (
     <div className='card'>
       <h3>
-        <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>
+        <a
+          className='text-dark'
+          href={repo.html_url}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           {repo.name}
         </a>
       </h3>
